b45ba1b863552205eba65c3ee93f9f7e1cd9ee01
Atsuki Demizu <demmys.atsuki@gmail.com>
2013-06-01 23:34:12 +0900

    check file format in format_file(main.c)

--- main.c
+++ main.c
@@ -7,13 +7,42 @@
 #define OUTPUT_EXTENSION ".class"
 #define OUTPUT_EXTENSION_LEN 6
 
+int format_name(char *file_name){
+    int classc = 0;
+    char *i;
+
+    // Skip path and get file name only.
+    for(i = file_name; *i; i++){
+        if(*i == '/')
+            file_name = i + 1;
+    }
+    // Check initial of file name(must be capital letter).
+    if(*file_name < 'A' || 'Z' < *file_name){
+        fprintf(stderr, "unsupported file name.\nfile name must begin with a capital letter.\n");
+        exit(1);
+    }
+    // Check and remove file extension.
+    for(i = file_name; *i; i++){
+        if(*i == '.'){
+            if(strcmp(i, INPUT_EXTENSION)){
+                fprintf(stderr, "unsupported file extension.\nfile extension must be \"%s\".\n", INPUT_EXTENSION);
+                exit(1);
+            }
+            *i = '\0';
+            return classc;
+        }
+        classc++;
+    }
+    fprintf(stderr, "unsupported file extension.\nfile extension must be \"%s\".\n", INPUT_EXTENSION);
+    exit(1);
+}
+
 int main(int argc, char *argv[]){
     extern int yyparse(void);
     extern FILE *yyin;
     Compiler *compiler;
     FILE *fp;
-    int classc = 0;
-    char *i;
+    int classc;
 
     // open file
     if(argc < 2){
@@ -25,34 +54,14 @@ int main(int argc, char *argv[]){
         exit(1);
     }
     yyin = fp;
-    for(i = argv[1]; *i; i++){
-        if(*i == '/')
-            argv[1] = i + 1;
-    }
-    if(*argv[1] < 'A' || 'Z' < *argv[1]){
-        fprintf(stderr, "unsupported file name.\nfile name must begin with a capital letter.\n");
-        exit(1);
-    }
-    for(i = argv[1]; *i; i++){
-        if(*i == '.'){
-            if(strcmp(i, INPUT_EXTENSION)){
-                fprintf(stderr, "unsupported file extension.\nfile extension must be \"%s\".\n", INPUT_EXTENSION);
-                exit(1);
-            }
-            *i = '\0';
-            break;
-        } else if(!*i){
-            fprintf(stderr, "unsupported file extension.\nfile extension must be \"%s\".\n", INPUT_EXTENSION);
-            exit(1);
-        }
-        classc++;
-    }
+
+    classc = format_name(argv[1]);
 
     compiler = create_compiler();
     if(yyparse()){
         compile_error(get_current_compiler() -> current_line_number, "parse error");
     }
-    //generate(argv[1], compiler);
+    //generate(classc, argv[1], compiler);
     dispose_compiler(compiler);
 
     return 0;
