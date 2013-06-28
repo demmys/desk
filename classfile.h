be43ff88913de6a34e3a95a1020ba22a7fda6e73
Atsuki Demizu <demmys.atsuki@gmail.com>
2013-06-29 00:15:05 +0900

    change classfile tag sort

--- classfile.h
+++ classfile.h
@@ -10,17 +10,17 @@ typedef unsigned int u4;
  * enumerated type
  */
 typedef enum{
-    CONSTANT_Class = 7,
-    CONSTANT_Fieldref = 9,
-    CONSTANT_Methodref = 10,
-    CONSTANT_InterfaceMethodref = 11,
-    CONSTANT_String = 8,
+    CONSTANT_Utf8 = 1,
     CONSTANT_Integer = 3,
     CONSTANT_Float = 4,
     CONSTANT_Long = 5,
     CONSTANT_Double = 6,
-    CONSTANT_NameAndType = 12,
-    CONSTANT_Utf8 = 1
+    CONSTANT_Class = 7,
+    CONSTANT_String = 8,
+    CONSTANT_Fieldref = 9,
+    CONSTANT_Methodref = 10,
+    CONSTANT_InterfaceMethodref = 11,
+    CONSTANT_NameAndType = 12
 } ConstantInfoTag;
 
 typedef enum{
