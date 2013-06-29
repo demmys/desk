c7e1dd6b4c758f5422cb9a4904385529cc007f3a
Atsuki Demizu <demmys.atsuki@gmail.com>
2013-06-29 11:22:01 +0900

    changed classfile.c

--- classfile.c
+++ classfile.c
@@ -6,18 +6,10 @@ ClassFile *get_current_classfile(){
     return static_current_classfile;
 }
 
-void *classfile_storage_malloc(size_t size){
-    void *p;
-    p = storage_malloc(static_current_classfile->classfile_storage, size);
-    return p;
-}
-
 ClassFile *create_class_file(){
-    Storage *storage;
     ClassFile *classfile;
 
-    storage = open_storage(0);
-    classfile = storage_malloc(storage, sizeof(ClassFile));
+    classfile = malloc(sizeof(ClassFile));
     classfile->magic = 0xcafebabe;
     classfile->minor_version = 0;
     classfile->major_version = 50;
@@ -33,7 +25,6 @@ ClassFile *create_class_file(){
     classfile->attributes_count = 1;
     classfile->source_file = NULL;
     classfile->emit_file = NULL;
-    classfile->classfile_storage = storage;
     static_current_classfile = classfile;
     return classfile;
 }
